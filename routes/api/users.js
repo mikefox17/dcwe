5e79d082eab1ea8408b0b4074faff9d5d5e34865
Michael Fox <mikefox17@gmail.com>
2019-08-12 01:40:43 -0400

    validation errors on posts

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 //@route Get api/users
-//@desc Test route
+//@desc Register User
 //@access Public
-router.get("/", (req, res) => res.send("USERS ROUTE..."));
+router.post(
+  "/",
+  [
+    check("name", "Name is required")
+      .not()
+      .isEmpty(),
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Password is required with a length of 6 characters"
+    ).isLength({ min: 6 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("USERS ROUTE...");
+  }
+);
 
 module.exports = router;
